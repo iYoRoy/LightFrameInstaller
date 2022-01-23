1f0aa6077f009a59bd5d9ee8fb6e3bdf52644836
翊冉 李 <3363384229@qq.com>
2022-01-23 16:25:48 +0800

    v0.1.1.10-β 使用https代替http

--- LightFrameInstaller.cpp
+++ LightFrameInstaller.cpp
@@ -475,7 +475,7 @@ void Panel1(HWND hWnd, HDC hdc)
 	}
 	TextPreDrawEx(hdc, 40, 120, 220, 24, L"最新版本(Total Build) :", 20, 0, VERTEXUICOLOR_WHITE);
 	TextPreDrawA(hdc, 240, 120, 200, 24, NewVer, VERTEXUICOLOR_WHITE);
-	TextPreDrawA(hdc, 10, 170, 200, 190, "Installer:v0.1.1.9-α", RGB(100, 100, 100));
+	TextPreDrawA(hdc, 10, 170, 200, 190, "Installer:v0.1.1.10-β", RGB(100, 100, 100));
 	CreateRect(hWnd, hdc, 0, 0, rc.right, 40, VERTEXUICOLOR_GREENDEEPSEA);
 	PanelDrawCloseBtn(hWnd, hdc, rc.right - 40, 0, 40, 40, 12, RGB(244, 244, 244));
 	PanelDrawOutFrame(hWnd, hdc, VERTEXUICOLOR_DARKENX);
